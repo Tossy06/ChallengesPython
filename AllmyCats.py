31d8b452676ce9647e07201a0fcde48ac844031e
David Gomez <davidgomez.sp06@gmail.com>
2024-06-21 13:41:06 -0500

    List of cat

---
+++ AllmyCats.py
@@ -0,0 +1,13 @@
+catName=[]
+
+while True:
+    print("Enter the name of cat "+ str(len(catName)+ 1) + "(Or enter nothing to stop): ")
+    name = input()
+    
+    if name == "":
+        break
+    catName = catName + [name]
+print("The cat name are: ")
+
+for name in catName:
+    print('  '+  name)
